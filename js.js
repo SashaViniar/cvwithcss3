1ad0cb4afa93a90a4bdeae61341aab029ae7ac9e
Sasha <vinar262@gmail.com>
2016-04-25 10:37:47 +0000

    cool page

---
+++ js.js
@@ -0,0 +1,31 @@
+// JavaScript File
+
+
+function square(a, b, c) {
+    var d = b * b - 4 * a * c;
+    var x1 = (-b + Math.sqrt(d)) / (2 * a);
+    var x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+    alert("x1 = " + x1);
+    alert("x2 = " + x2);
+}
+
+square(1, 5, 0);
+
+function fib(stopnumber) {
+    var f0 = 1;
+    var f1 = 2;
+    var f2 = f1 + f0;
+    var result = ' ';
+    while (f2 < stopnumber){
+        f2 = f1 + f0;
+
+        result += '\n' + f2;
+
+        f0 = f1;
+        f1 = f2;
+    }
+    alert(result);
+}
+
+fib(120);
